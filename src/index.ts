e05c3a144bc620dca0e3562150c5132bf86cc93a
Javier Morales de Vera <javierMorales9@gmail.com>
2022-07-06 00:36:15 +0200

    Add coinbase sdk

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
+import dotenv from "dotenv";
+import {CoinbasePro} from 'coinbase-pro-node';
 
-console.log(process.env.NODE_ENV);
+dotenv.config();
+
+const apiKey = process.env.COINBASE_API_KEY;
+const apiSecret = process.env.COINBASE_API_SECRET;
+const apiPassPhrase = process.env.COINBASE_API_PASPHRASE;
+
+if (!apiKey) throw new Error("Invalid apiKey");
+if (!apiSecret) throw new Error("Invalid apiSecret");
+if (!apiPassPhrase) throw new Error("Invalid PassPhrase");
+
+const client = new CoinbasePro({
+  apiKey,
+  apiSecret,
+  passphrase: apiPassPhrase,
+  useSandbox: true,
+});
+
+client.rest.account.listAccounts()
+.then(accounts => {
+console.log(accounts);
+});
