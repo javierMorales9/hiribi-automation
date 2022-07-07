755346f6ed37e272c69ab206f0cb60aea15f171b
Javier Morales de Vera <javierMorales9@gmail.com>
2022-07-07 20:43:07 +0200

    basic coinbase api request

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
+import {CoinbasePro} from 'coinbase-pro-node';
+import dotenv from "dotenv";
 
-console.log(process.env.NODE_ENV);
+dotenv.config();
+
+if(!process.env.COINBASE_API_KEY) throw new Error("Incorrect api key");
+if(!process.env.COINBASE_API_SECRET) throw new Error("Incorrect api secret");
+if(!process.env.COINBASE_API_PASPHRASE) throw new Error("Incorrect api passphrase");
+
+const client = new CoinbasePro({
+    apiKey: process.env.COINBASE_API_KEY,
+    apiSecret: process.env.COINBASE_API_SECRET,
+    passphrase: process.env.COINBASE_API_PASPHRASE,
+    useSandbox: true,
+});
+
+client.rest.account.listAccounts().then(accounts => {
+  console.log(accounts);
+});
