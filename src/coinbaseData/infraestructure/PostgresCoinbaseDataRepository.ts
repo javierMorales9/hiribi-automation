e2e2a7e45151555e61c9a9efd1a5cb07d0731cdd
Javier Morales de Vera <javierMorales9@gmail.com>
2022-07-31 23:39:59 +0200

    Implemented PostgresCoinbaseDataRepository.ts

---
+++ src/coinbaseData/infraestructure/PostgresCoinbaseDataRepository.ts
@@ -0,0 +1,38 @@
+import CoinbaseDataRepository from "../domain/CoinbaseDataRepository";
+import CoinbaseData from "../domain/CoinbaseData";
+import CoinbaseDataModel from "./CoinbaseDataModel";
+
+export default class PostgresCoinbaseDataRepository implements CoinbaseDataRepository{
+    public async create(coinbaseData: CoinbaseData): Promise<CoinbaseData> {
+        const coinbaseDataModel = await CoinbaseDataModel.create({
+            apiKey: coinbaseData.apiKey,
+            apiSecret: coinbaseData.apiSecret,
+            apiPassPhrase: coinbaseData.apiPassPhrase,
+            portfolioId: coinbaseData.portfolioId
+        })
+
+        return this.transformToCoinbaseData(coinbaseDataModel);
+    }
+
+    public async get(id: string): Promise<CoinbaseData | null> {
+        const coinbaseDataModel = await CoinbaseDataModel.findByPk(parseInt(id));
+        return this.handleFinding(coinbaseDataModel);
+    }
+
+    private handleFinding(coinbaseDataModel: CoinbaseDataModel | null): CoinbaseData | null{
+        if(!coinbaseDataModel)
+            return null
+
+        return this.transformToCoinbaseData(coinbaseDataModel);
+    }
+
+    private transformToCoinbaseData(coinbaseDataModel: CoinbaseDataModel): CoinbaseData {
+        return new CoinbaseData(
+            coinbaseDataModel.apiKey,
+            coinbaseDataModel.apiSecret,
+            coinbaseDataModel.apiPassPhrase,
+            coinbaseDataModel.portfolioId,
+            coinbaseDataModel.id.toString()
+        );
+    }
+}
